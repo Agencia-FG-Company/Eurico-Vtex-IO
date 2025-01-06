47898194d038184866f0c40cc350e723e5f2a5d3
Lislayne <lis.figueira@agenciafg.com.br>
2025-01-06 12:59:33 -0300

    ajustes gerais

--- app-custom/react/components/CustomButtonsBrands/CustomButtonsBrands.tsx
+++ app-custom/react/components/CustomButtonsBrands/CustomButtonsBrands.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { useListContext, ListContextProvider } from "vtex.list-context";
-import RichText from 'vtex.rich-text/index'
+import { RichText } from 'vtex.rich-text';
 
-import styles from "./styles.css"
+import styles from "./styles.css";
 
 import ButtonBrand from "./ButtonBrand";
 import { IButtonBrand } from "./types/ButtonBrandType";
@@ -12,26 +12,32 @@ const CustomButtonsBrands = (props: IButtonsBrand) => {
   const { list } = useListContext() || [];
   const [item1] = props.children;
 
-  if(!props.isActive) return <></>
+  if (!props.isActive) return <></>;
 
-  const listContent = props.buttonsContent.map((button: IButtonBrand, index: number) => (
-    <ButtonBrand categorySrc={button.categorySrc} imageSrc={button.imageSrc} key={index}/>
-  ))
+  const listContent = props.buttonsContent.map(
+    (button: IButtonBrand, index: number) => (
+      <ButtonBrand
+        categorySrc={button.categorySrc}
+        imageSrc={button.imageSrc}
+        key={index}
+      />
+    )
+  );
   const newListContextValue = list.concat(listContent);
 
   return (
     <div className={styles.CustomButtonsBrands}>
+      {/* Verifique o uso correto do RichText */}
       <RichText text={props.title} />
       <ListContextProvider list={newListContextValue}>
-          {item1}
+        {item1}
       </ListContextProvider>
     </div>
-  )
-
-}
+  );
+};
 
 export default CustomButtonsBrands;
 
 CustomButtonsBrands.schema = {
-  title: "Lista de Botões de Fornecedores"
-}
+  title: "Lista de Botões de Fornecedores",
+};
--- app-custom/react/components/CustomButtonsSize/CustomButtonsSize.tsx
+++ app-custom/react/components/CustomButtonsSize/CustomButtonsSize.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useListContext, ListContextProvider } from "vtex.list-context";
-import RichText from 'vtex.rich-text/index'
+// import RichText from 'vtex.rich-text/index'
 
 import styles from "./styles.css"
 
@@ -12,18 +12,18 @@ const CustomButtonsSize = (props: ICustomButtonsSize) => {
   const { list } = useListContext() || [];
   const [item1] = props.children;
 
-  if(!props.isActive) return <></>
+  if (!props.isActive) return <></>
 
   const listContent = props.buttonsContent.map((button: IButtonSize, index: number) => (
-    <ButtonSize sizeText={button.sizeText} categorySrc={button.categorySrc} key={index}/>
+    <ButtonSize sizeText={button.sizeText} categorySrc={button.categorySrc} key={index} />
   ))
   const newListContextValue = list.concat(listContent);
 
   return (
     <div className={styles.CustomButtonsSize}>
-      <RichText text={props.title} />
+      {/* <RichText text={props.title} /> */}
       <ListContextProvider list={newListContextValue}>
-          {item1}
+        {item1}
       </ListContextProvider>
     </div>
   )
